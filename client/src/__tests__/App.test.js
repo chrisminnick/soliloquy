7f1d553291eec8cd584dfafbab64f73adf981522
Chris Minnick <chris@minnick.com>
2023-12-19 12:18:43 -0800

    added test

---
+++ client/src/__tests__/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '../components/App';
+
+describe('App', () => {
+  test('renders App component', () => {
+    render(<App />);
+  });
+});
